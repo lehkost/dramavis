80ceeb0e75059d6ed3d76cd4f6f6af5073b6a881
lehkost <fleance@gmx.net>
2015-06-24 18:35:35 +0200

    added comments

--- output_(666_erroneous_graphs_from_tg-rep)/superposter.sh
+++ output_(666_erroneous_graphs_from_tg-rep)/superposter.sh
@@ -10,21 +10,25 @@
 # http://www.csb.yale.edu/userguides/image/im/www/montage.html
 # http://www.imagemagick.org/Usage/montage/#geometry_size
 
+# delete old superposter
 if [ -e superposter.png ]
 then
    rm superposter.png
 fi
 
+# start with plays written B.C. (sort backwards)
 for n in `ls BC*.png | sort -r`
 do
 	filelist=$filelist$n' '
 done
 
+# add plays written A.D.
 for n in `ls [0-9]*.png | sort`
 do
 	filelist=$filelist$n' '
 done
 
-montage -monitor -tile 10x70 -geometry 100x100+1+1 $filelist superposter.png
+# hand over filelist to ImageMagick's montage feature
+montage -monitor -tile 29x23 -geometry 600x600+1+1 $filelist superposter.png
 
 echo
